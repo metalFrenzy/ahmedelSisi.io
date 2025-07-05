39bedc453d2851e164bd4837e359cde69530a701
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-05 22:03:10 +0300

    feat(ui): add landing and make the font cabin across the whole project

---
+++ src/app/components/landing/landing.tsx
@@ -0,0 +1,16 @@
+
+export default function landing() {
+    return (
+        <div className="cover-page">
+            <div className="text-overlay">
+                <h1>Ahmed El Sisi, Software Engineer</h1>
+                <p>I dont need other people approval and I dont need everyone to like, and always give yourself value </p>
+                <div className="action-buttons">
+                    <button>View projects</button>
+                    <button>Download resume</button>
+                </div>
+            </div>
+        </div>
+    )
+
+}
