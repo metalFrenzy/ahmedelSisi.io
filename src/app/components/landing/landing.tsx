60ee2070af27fe49f32985e47e132da28c895d12
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-06 21:56:46 +0300

    feat(ui): add the landing page

--- src/app/components/landing/landing.tsx
+++ src/app/components/landing/landing.tsx
@@ -1,13 +1,19 @@
+import './landing.scss'
 
 export default function landing() {
     return (
         <div className="cover-page">
             <div className="text-overlay">
-                <h1>Ahmed El Sisi, Software Engineer</h1>
-                <p>I dont need other people approval and I dont need everyone to like, and always give yourself value </p>
+                <h1 className="title">
+                    Ahmed El Sisi<br />
+                    <span className="subtitle">Software Engineer</span>
+                </h1>
+                <p>I think like a designer, build like an engineer, and ship like I’ve got something to prove.</p>
                 <div className="action-buttons">
-                    <button>View projects</button>
-                    <button>Download resume</button>
+                    <button className='view'>View projects</button>
+                    <a href="/assets/Ahmed-El-Sisi_CV.pdf" download className="download">
+                        Download resume
+                    </a>
                 </div>
             </div>
         </div>
