536aa0611e24c79f7d5779df6951677dce277c83
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-09 01:00:15 +0300

    feat(ui): add the about me section

---
+++ src/app/components/about-me/about-me.tsx
@@ -0,0 +1,16 @@
+import './about-me.scss'
+export default function AboutMe() {
+    return (
+        <div className="about-me-container">
+            <img src="assets/mee.svg" alt="" />
+            <div className="text">
+                <h1>About me</h1>
+                <p>
+                    I’m a Software Engineer who enjoys building things from the ground up — especially when they’re complex or challenging. I’m most at home working full-stack with tools like NestJS, Angular, PostgreSQL, Flutter, and Figma.
+                    I approach problems with structure: I break ideas down visually in my notes, plan them step by step, and then build with clean code and solid architecture. What sets me apart isn’t just my ability to code — it’s how I think. I bring real software engineering practices into everything I do — from user stories and Git workflows to testing and maintainability.
+                    I’m also adaptable. I’ve taken on tasks in unfamiliar tech — like learning Docker from scratch and containerizing a full-stack app in just two days — because I’m always ready to figure things out and get the job done.
+                </p>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutMe from "./components/about-me/about-me";
 import Landing from "./components/landing/landing";
 
 export default function Home() {
   return (
     <main>
-      <Landing/>
+      <Landing />
+      <AboutMe />
     </main>
   )
 }
