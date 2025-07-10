9715329e48c51954844d632e1968edca2cacc123
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-10 03:26:21 +0300

    feat(ui): add the tech stack and skills section

---
+++ src/app/components/tech-stack/tech-stack.tsx
@@ -0,0 +1,28 @@
+import './tech-stack.scss';
+
+export default function TechStack() {
+    return (
+        <div className="tech-stack">
+            <h1 className='tech'>Tech Stack & Skills</h1>
+            <div className='stack-scroll'>
+            <div className='stack'>
+            <img src="https://cdn.simpleicons.org/html5/e34f26" alt="HTML5" />
+             <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/css3.svg" alt="CSS3" />
+            <img src="https://cdn.simpleicons.org/typescript/3178C6" alt="TypeScript" />
+            <img src="https://cdn.simpleicons.org/javascript/F7DF1E" alt="JavaScript" />
+            <img src="https://cdn.simpleicons.org/nestjs/ed2235" alt="NestJS" />
+            <img src="https://cdn.simpleicons.org/node.js/339933" alt="Node.js" />
+            <img src="https://cdn.simpleicons.org/angular/DD0031" alt="Angular" />
+            <img src="https://cdn.simpleicons.org/flutter/02569B" alt="Flutter" />
+            <img src="https://cdn.simpleicons.org/next.js/000000" alt="Next.js" />
+            <img src="https://cdn.simpleicons.org/sass/CC6699" alt="SCSS" />
+            <img src="https://cdn.simpleicons.org/git/F05032" alt="Git" />
+            <img src="https://cdn.simpleicons.org/github/181717" alt="GitHub" />
+            <img src="https://cdn.simpleicons.org/postgresql/4169E1" alt="PostgreSQL" />
+            <img src="https://cdn.simpleicons.org/firebase/FFCA28" alt="Firebase" />
+            <img src="https://cdn.simpleicons.org/docker/2496ED" alt="Docker" />
+            </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "./components/about-me/about-me";
 import Landing from "./components/landing/landing";
+import TechStack from "./components/tech-stack/tech-stack";
 
 export default function Home() {
   return (
     <main>
       <Landing />
       <AboutMe />
+      <TechStack />
     </main>
   )
 }
